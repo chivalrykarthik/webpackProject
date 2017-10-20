8e3d29d73910facdbe8ae25548ed6da17e8b3529
Karthik Arumugam <chivalrykarthik@gmail.com>
2017-10-20 21:45:25 +0530

    Code movement

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+class parent{
+    constructor(){
+        console.log("parent constructor called");
+    }
+    add(a,b){
+        return a+b;
+
+    }
+}
+
+
+
+class child extends parent{
+    constructor(){
+        console.log("My first webpack app updatedNO:5");
+        super();
+    }
+}
+
+
+
+
+let obj = new child();
+console.log(obj.add(1,2));
